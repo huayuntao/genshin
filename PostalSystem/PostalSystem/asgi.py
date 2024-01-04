fcdebf22ab4ededb1f3e23faef3d2555a3e6b0a7
xjsc01 <109519743+xjsc01@users.noreply.github.com>
2024-01-04 11:07:45 +0800

    last

--- PostalSystem/PostalSystem/asgi.py
+++ PostalSystem/PostalSystem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GenshinPostalSystem project.
+ASGI config for PostalSystem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GenshinPostalSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PostalSystem.settings')
 
 application = get_asgi_application()
--- PostalSystem/PostalSystem/wsgi.py
+++ PostalSystem/PostalSystem/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GenshinPostalSystem project.
+WSGI config for PostalSystem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GenshinPostalSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PostalSystem.settings')
 
 application = get_wsgi_application()
